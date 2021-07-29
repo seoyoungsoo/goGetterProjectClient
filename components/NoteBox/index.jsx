03ebe8a12d317db5a703597270e0ab0419c86c01
seoyoungsoo <iulove37@naver.com>
2021-07-30 01:37:51 +0900

    Feat #98: 통신 컴포넌트 변경

--- components/NoteBox/index.jsx
+++ components/NoteBox/index.jsx
@@ -1,15 +1,16 @@
-import React, { useEffect, useCallback, useState } from 'react';
+import React, { useEffect, useCallback, useState, useRef } from 'react';
 import { Container, Content } from '@components/NoteBox/styles';
 import Member from '@components/NoteBox/Member';
-import Detail from '@components/NoteBox/Detail';
+import moment from 'moment';
 import apiController from '@apis/apiController';
 import { useSelector, useDispatch } from 'react-redux';
 import { insertPartner, insertMessage, receive } from '@reducers/conversation';
+import SockJsClient from 'react-stomp';
 
-const NoteBox = ({ userId, sendToMessage }) => {
+const NoteBox = ({ userId }) => {
   const conversationList = useSelector((state) => state.conversation);
 
-  // const [memberList, setMemberList] = useState([]);
+  const [memberList, setMemberList] = useState([]);
   const [partner, setPartner] = useState('');
   const [roomId, setRoomId] = useState('');
   const [nickName, setNickName] = useState('');
@@ -27,7 +28,7 @@ const NoteBox = ({ userId, sendToMessage }) => {
     }).then((res) => {
       if (res.data.message === '조회성공') {
         // console.log(res.data.data);
-        // setMemberList(res.data.data);
+        setMemberList(res.data.data);
         for (const key in res.data.data) {
           dispatch(
             insertPartner({
@@ -47,31 +48,68 @@ const NoteBox = ({ userId, sendToMessage }) => {
     setNickName(nickName);
   }, []);
 
+  const socketRef = useRef(null);
+  let topics = ['/topic/' + userId];
+
+  const sendToMessage = (senderId, receiverId, roomId, chat, nickName) => {
+    const nowTime = moment().format('YYYY-MM-DD HH:mm:ss');
+
+    const reduxParams = {
+      the_other_user_id: receiverId,
+      nick_name: nickName,
+      content: chat,
+      send_at: nowTime,
+      message_room_id: roomId,
+    };
+    const socketParams = {
+      sender_id: senderId,
+      receiver_id: receiverId,
+      message_room_id: roomId,
+      content: chat,
+    };
+    socketRef.current.sendMessage('/app/chat/send', JSON.stringify(socketParams));
+    dispatch(insertMessage(reduxParams));
+  };
+
+  const receiveMessage = (msg) => {
+    console.log('receive!' + msg);
+    // dispatch(receive(msg));
+  };
+
   return (
     <div>
       <Container>
         <Content>
           <h4>채팅목록</h4>
-          {/* {memberList?.map((data, index) => (
-            <React.Fragment key={index}>
-              <Member data={data} />
-            </React.Fragment>
-          ))} */}
-          {Object.keys(conversationList).map((key) => (
-            <Member
-              key={key}
-              partner={key}
-              host={userId}
-              onClickMember={onClickMember}
-              roomId={conversationList[key][0].message_room_id}
-              nickName={conversationList[key][0].nick_name}
-            />
-          ))}
+          {Object.keys(conversationList).map(
+            (key, index) => (
+              <Member
+                key={key}
+                // partner={key}
+                host={userId}
+                // onClickMember={onClickMember}
+                // roomId={conversationList[key][0].message_room_id}
+                // nickName={conversationList[key][0].nick_name}
+                sendToMessage={sendToMessage}
+                partner={memberList[index].the_other_user_id}
+                nickName={memberList[index].nick_name}
+                roomId={memberList[index].message_room_id}
+              />
+            ),
+            // console.log(conversationList, key, index),
+          )}
         </Content>
-        {partner && (
-          <Detail sendToMessage={sendToMessage} userId={userId} partner={partner} roomId={roomId} nickName={nickName} />
-        )}
       </Container>
+      <SockJsClient
+        url="/chat"
+        topics={topics}
+        onConnect={() => console.log('onConnect')}
+        onDisconnect={() => console.log('onDisconnect')}
+        onMessage={(msg) => {
+          receiveMessage(msg);
+        }}
+        ref={socketRef}
+      />
     </div>
   );
 };
