47b2f63af19ddf3a5684d86c8db531f7a254697b
seoyoungsoo <iulove37@naver.com>
2021-07-30 01:38:13 +0900

    Feat #97: 멤버 클릭시 채팅 모달 출현 구현

--- components/NoteBox/Member/index.jsx
+++ components/NoteBox/Member/index.jsx
@@ -1,24 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Container, Top, Bottom } from '@components/NoteBox/Member/styles';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import formatDate from '@utils/formatDate';
+import MessageList from '../MessageList';
+import apiController from '@apis/apiController';
+import { chatList } from '@reducers/conversation';
 
-const Member = ({ partner, host, onClickMember, roomId, nickName }) => {
-  const data = useSelector((state) => state.conversation[partner][state.conversation[partner].length - 1]);
+const Member = ({ partner, host, roomId, nickName, sendToMessage }) => {
+  const data = useSelector((state) => state.conversation);
+  const conversationList = data[partner][data[partner].length - 1];
 
-  const sendMemberData = () => {
-    onClickMember(partner, roomId, nickName);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getChatList();
+  }, []);
+
+  const getChatList = useCallback(() => {
+    apiController({
+      url: `/users/chats/detail/${roomId}`,
+      method: 'get',
+    }).then((res) => {
+      // console.log(res.data.data);
+      for (const key in res.data.data) {
+        let params = {
+          the_other_user_id: partner,
+          nick_name: res.data.data[key].send_nick_name,
+          content: res.data.data[key].content,
+          send_at: res.data.data[key].send_at,
+          message_room_id: roomId,
+        };
+
+        dispatch(
+          chatList({
+            partner: partner,
+            list: params,
+          }),
+        );
+      }
+    });
+  }, [partner, roomId, nickName]);
+
+  const [showMessage, setShowMessage] = useState(false);
+
+  const openMessage = () => {
+    setShowMessage(true);
   };
 
   return (
     <div>
-      <Container onClick={sendMemberData}>
+      <Container onClick={openMessage}>
         <Top>
-          <h5>{data.nick_name}</h5>
-          <span>{formatDate(data.send_at)}</span>
+          <h5>{conversationList.nick_name}</h5>
+          <span>{formatDate(conversationList.send_at)}</span>
         </Top>
-        <Bottom>{data.content}</Bottom>
+        <Bottom>{conversationList.content}</Bottom>
       </Container>
+      <MessageList
+        showMessage={showMessage}
+        setShowMessage={setShowMessage}
+        partner={partner}
+        host={host}
+        list={data[partner]}
+        sendToMessage={sendToMessage}
+        roomId={roomId}
+        nickName={nickName}
+      />
     </div>
   );
 };
