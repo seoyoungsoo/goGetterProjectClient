e1e0f5dbda93592875e6e5f7ed46805c971fec55
seoyoungsoo <iulove37@naver.com>
2021-07-29 22:42:35 +0900

    Feat #100: 시간 변경

--- components/NoteBox/Member/index.jsx
+++ components/NoteBox/Member/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Top, Bottom } from '@components/NoteBox/Member/styles';
 import { useSelector } from 'react-redux';
+import formatDate from '@utils/formatDate';
 
 const Member = ({ partner, host, onClickMember, roomId, nickName }) => {
   const data = useSelector((state) => state.conversation[partner][state.conversation[partner].length - 1]);
@@ -14,7 +15,7 @@ const Member = ({ partner, host, onClickMember, roomId, nickName }) => {
       <Container onClick={sendMemberData}>
         <Top>
           <h5>{data.nick_name}</h5>
-          <span>{data.send_at}</span>
+          <span>{formatDate(data.send_at)}</span>
         </Top>
         <Bottom>{data.content}</Bottom>
       </Container>
