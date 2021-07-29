8e84a2307a1b1a79573ac09ab7dfa0101d7b8f1a
seoyoungsoo <iulove37@naver.com>
2021-07-29 22:41:56 +0900

    Feat #101: 실시간 채팅 구현

--- components/NoteBox/Detail/ChatList/index.jsx
+++ components/NoteBox/Detail/ChatList/index.jsx
@@ -1,31 +1,17 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { ChatZone } from '@components/NoteBox/Detail/ChatList/styles';
-import { Scrollbars } from 'react-custom-scrollbars';
+import Scrollbars from 'react-custom-scrollbars';
 import Chat from '@components/NoteBox/Detail/Chat';
 
-const ChatList = () => {
+const ChatList = ({ roomId, chatData, setChatData }) => {
+  const chatSections = chatData ? chatData.flat().reverse() : [];
+
   return (
     <ChatZone>
       <Scrollbars autoHide>
-        <Chat />
-        <Chat />
-        <Chat />
-        <Chat />
-        <Chat />
-        <Chat />
-        <Chat />
-        <Chat />
-        <Chat />
-        <Chat />
-        <Chat />
-        <Chat />
-        <Chat />
-        <Chat />
-        <Chat />
-        <Chat />
-        <Chat />
-        <Chat />
-        <Chat />
+        {chatSections?.map((chat) => (
+          <Chat key={chat.send_at} data={chat} />
+        ))}
       </Scrollbars>
     </ChatZone>
   );
