4e1b4d96798eb46d4ce153a3ceac372a2e1d105f
seoyoungsoo <iulove37@naver.com>
2021-07-29 22:41:14 +0900

    Feat #101: 채팅 상세 조회 구현

--- components/NoteBox/Detail/index.jsx
+++ components/NoteBox/Detail/index.jsx
@@ -1,26 +1,31 @@
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useRef, useState, useEffect } from 'react';
 import { Container, Top } from '@components/NoteBox/Detail/styles';
 import ChatBox from '@components/NoteBox/Detail/ChatBox';
 import ChatList from '@components/NoteBox/Detail/ChatList';
 import useInput from '@hooks/useInput';
-import { useSelector } from 'react-redux';
-// import SockJsClient from 'react-stomp';
+import apiController from '@apis/apiController';
 
 const Detail = ({ sendToMessage, userId, partner, roomId, nickName }) => {
   const [chat, onChangeChat, setChat] = useInput('');
+  const [chatData, setChatData] = useState([]);
 
   const onSubmitForm = useCallback(
     (e) => {
       e.preventDefault();
-      console.log(chat);
+      console.log(userId, partner, roomId, chat, nickName);
       // 통신
-      sendToMessage(userId, partner, roomId, chat);
+      sendToMessage(userId, partner, roomId, chat, nickName);
       setChat('');
     },
-    [userId, chat],
+    [userId, partner, roomId, chat, nickName],
   );
 
-  const receiveMessage = (msg) => {};
+  useEffect(() => {
+    apiController({
+      url: `users/chats/detail/${roomId}`,
+      method: 'get',
+    }).then((res) => setChatData(res.data.data));
+  }, []);
 
   return (
     <div>
@@ -28,7 +33,7 @@ const Detail = ({ sendToMessage, userId, partner, roomId, nickName }) => {
         <Top>
           <h4>닉네임</h4>
         </Top>
-        <ChatList />
+        <ChatList roomId={roomId} chatData={chatData} setChatData={setChatData} />
         <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
       </Container>
     </div>
