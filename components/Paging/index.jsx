070826569d0d415493139a7d2e15b4cf24aba176
seoyoungsoo <iulove37@naver.com>
2021-07-30 17:37:16 +0900

    Fix #noissue: 페이징 오류 해결

--- components/Paging/index.jsx
+++ components/Paging/index.jsx
@@ -5,39 +5,38 @@ import Pagination from 'react-js-pagination';
 const Paging = ({ bookBoard = null, discussion = null, totalElements, currentPage, handlePageChange }) => {
   return (
     <div>
-      {bookBoard ? (
-        <Pagination
-          activePage={currentPage + 1}
-          itemsCountPerPage={8}
-          totalItemsCount={totalElements}
-          pageRangeDisplayed={5}
-          prevPageText={'<'}
-          nextPageText={'>'}
-          onChange={handlePageChange}
-        />
-      ) : ( discussion ? (
-        <Pagination
-          activePage={currentPage + 1}
-          itemsCountPerPage={7}
-          totalItemsCount={totalElements}
-          pageRangeDisplayed={10}
-          prevPageText={'<'}
-          nextPageText={'>'}
-          onChange={handlePageChange}
-        />
-      ) :(
-        <Pagination
-        activePage={currentPage + 1}
-        itemsCountPerPage={20}
-        totalItemsCount={totalElements}
-        pageRangeDisplayed={10}
-        prevPageText={'<'}
-        nextPageText={'>'}
-        onChange={handlePageChange}
-      />
-      ) 
-      )
-  }
+      {totalElements &&
+        (bookBoard ? (
+          <Pagination
+            activePage={currentPage + 1}
+            itemsCountPerPage={8}
+            totalItemsCount={totalElements}
+            pageRangeDisplayed={5}
+            prevPageText={'<'}
+            nextPageText={'>'}
+            onChange={handlePageChange}
+          />
+        ) : discussion ? (
+          <Pagination
+            activePage={currentPage + 1}
+            itemsCountPerPage={7}
+            totalItemsCount={totalElements}
+            pageRangeDisplayed={10}
+            prevPageText={'<'}
+            nextPageText={'>'}
+            onChange={handlePageChange}
+          />
+        ) : (
+          <Pagination
+            activePage={currentPage + 1}
+            itemsCountPerPage={20}
+            totalItemsCount={totalElements}
+            pageRangeDisplayed={10}
+            prevPageText={'<'}
+            nextPageText={'>'}
+            onChange={handlePageChange}
+          />
+        ))}
     </div>
   );
 };
