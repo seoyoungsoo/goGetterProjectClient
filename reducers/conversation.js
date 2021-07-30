5a132416aec7e50f8493f81d5dd932430daaa6d6
seoyoungsoo <iulove37@naver.com>
2021-07-30 22:14:37 +0900

    Feat #noissue: 실시간 채팅 관련 리덕스 Receive 관련 내용 추가

--- reducers/conversation.js
+++ reducers/conversation.js
@@ -1,10 +1,12 @@
 const NEWPARTNER = 'conversation/NEWPARTNER';
 const NEWMESSAGE = 'conversation/NEWMESSAGE';
 const RECEIVED = 'conversation/RECEIVED';
+const CHATLIST = 'conversation/ADDCHATLIST';
 
 export const insertPartner = (partner) => ({ type: NEWPARTNER, payload: partner });
 export const insertMessage = (msg) => ({ type: NEWMESSAGE, payload: msg });
 export const receive = (msg) => ({ type: RECEIVED, payload: msg });
+export const conversationList = (partner) => ({ type: CHATLIST, payload: partner });
 
 const initialState = [];
 
@@ -14,18 +16,22 @@ const Conversation = (state = initialState, action) => {
       // console.log(action.payload.list);
       state[action.payload.partner] = [...action.payload.list];
       return { ...state };
+    case CHATLIST:
+      // console.log(action.payload);
+      state[action.payload.partner] = [...state[action.payload.partner], action.payload.list];
+      return { ...state };
     case NEWMESSAGE:
       // console.log(action.payload);
       state[action.payload.the_other_user_id] = [...state[action.payload.the_other_user_id], action.payload];
       return { ...state };
     case RECEIVED:
-    // console.log(action.payload);
-    // if (state[action.payload.author] === undefined) {
-    //   state[action.payload.author] = [action.payload];
-    // } else {
-    //   state[action.payload.author] = [...state[action.payload.author], action.payload];
-    // }
-    // return { ...state };
+      // console.log(action.payload);
+      if (state[action.payload.the_other_user_id] === undefined) {
+        state[action.payload.the_other_user_id] = [action.payload];
+      } else {
+        state[action.payload.the_other_user_id] = [...state[action.payload.the_other_user_id], action.payload];
+      }
+      return { ...state };
     default:
       return state;
   }
